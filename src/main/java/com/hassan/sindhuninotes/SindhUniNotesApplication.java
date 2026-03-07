e8cba1d13642c3985a19ceb5166de282de4ddfc6
HassanMallah <hassanmallah02@gmail.com>
2026-03-07 21:04:42 +0500

    initial commit

---
+++ src/main/java/com/hassan/sindhuninotes/SindhUniNotesApplication.java
@@ -0,0 +1,13 @@
+package com.hassan.sindhuninotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SindhUniNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SindhUniNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hassan/sindhuninotes/SindhUniNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hassan.sindhuninotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SindhUniNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
